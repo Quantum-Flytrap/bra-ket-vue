3debad92a52ba28c1ba65878d74b6fa215084354
Klem <k.jankiewicz@gmail.com>
2020-01-28 15:06:48 +0100

    other buttons

--- src/lib-components/viewer-button.vue
+++ src/lib-components/viewer-button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button :class="computeClass">
+  <button>
     <slot></slot>
   </button>
 </template>
@@ -8,69 +8,41 @@
 import { Vue, Component, Prop } from 'vue-property-decorator';
 @Component
 export default class ViewerButton extends Vue {
-  @Prop() readonly hoverColor!: string
-
   @Prop({ default: 'basic' }) readonly type!: string
-
-  @Prop({ default: true }) readonly inline!: boolean
-
-  @Prop() readonly overlay!: boolean
-
-  get computeClass(): string[] {
-    return [this.type, this.inline ? 'inline' : '', this.overlay ? 'overlay-btn' : ''];
-  }
 }
 </script>
 
 <style scoped lang="scss">
 button {
   display: inline-block;
-  outline: white;
-  border-width: 1px;
   font-family: 'Montserrat', Helvetica, Arial, sans-serif;
-  background-color: transparent;
+  background-color: rgba(255, 255, 255, 0.1);
   cursor: pointer;
   color: white;
-  padding: 6px 12px;
+  padding: 4px 12px;
   text-align: center;
   text-decoration: none;
   text-transform: uppercase;
-  font-size: 0.6rem;
+  font-size: 9px;
   transition: 0.5s;
-  border-radius: 20px;
   margin: 3px;
-  &.inline {
-    display: inline;
-  }
+  border: none;
   &.basic {
     font-size: 9px;
     &:hover {
-      background: rgba(255, 255, 255, 0.1);
-      transition: 0.5s;
-    }
-    &:focus {
-      background: rgba(255, 255, 255, 0.1);
+      background: rgba(255, 255, 255, 0.2);
     }
   }
   &.icon {
-    font-size: 18px;
+    font-size: 13px;
     line-height: 5px;
-    //padding: 3px 10px;
-    height: 25px;
-    width: 70px;
+    height: 19px;
+    width: 55px;
     padding: none;
     &:hover {
-      background: rgba(255, 255, 255, 0.1);
-      transition: 0.5s;
+      background: rgba(255, 255, 255, 0.2);
       text-decoration: none;
     }
-    &:focus {
-      background: rgba(255, 255, 255, 0.1);
-    }
-  }
-  &.overlay-btn {
-    margin: 5px;
-    text-decoration: none;
   }
 }
 </style>
