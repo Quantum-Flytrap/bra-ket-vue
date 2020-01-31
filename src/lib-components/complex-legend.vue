88fdb4b8d3c53558d46492e02f06cf6ed58b76fa
Piotr Migdal <pmigdal@gmail.com>
2020-01-31 02:03:01 +0100

    refactor(matrix) complex legend component

---
+++ src/lib-components/complex-legend.vue
@@ -0,0 +1,127 @@
+<template>
+  <svg class="quantum-matrix"
+    :width="300"
+    :height="100">
+      <g class="legend" :transform="`translate(35, 20)`">
+        <g>
+          <text class="label" :x="0" :y="0">
+            Amplitude
+          </text>
+          <circle class="radius-reference" :cx="scale(0)" :cy="scale(1)" :r="rScale(1)" />
+          <circle
+            class="radius-value"
+            :cx="scale(0)"
+            :cy="scale(1)"
+            :r="selectedNonzero ? rScale(re, im) : rScale(1)"
+          />
+          <text v-if="selectedNonzero" class="label" :x="rScale(0)" :y="scale(2)">
+            {{ Math.sqrt(re ** 2 + im ** 2 || 0).toFixed(3) }}
+          </text>
+        </g>
+        <g :transform="`translate(${scale(3)}, 0)`">
+          <text class="label" :x="0" :y="0">
+            Phase
+          </text>
+          <g :transform="`translate(0, ${scale(1)})`">
+            <path
+              v-for="(a, i) in arcs"
+              :key="`arc-${i}`"
+              class="phase-arc"
+              :style="{
+                fill: generateColor(a.re, a.im),
+                opacity: !selectedNonzero || selectedEntryPhaseId === i ? 1 : 0.25
+              }"
+              :d="`M 0 0 L ${a.x0} ${a.y0} A ${rScale(1)} ${rScale(1)} 0 0 1 ${a.x1} ${a.y1} Z`"
+            />
+          </g>
+          <text v-if="selectedNonzero" class="label" :x="rScale(0)" :y="scale(2)">
+            {{ selectedPhaseTau.toFixed(2) }} 𝛕
+          </text>
+        </g>
+      </g>
+    </svg>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { range } from '@/lib-components/utils';
+import { colorComplex } from '@/lib-components/colors';
+
+
+@Component({
+  components: {},
+})
+
+export default class ComplexLegend extends Vue {
+  @Prop({ default: () => 40 }) private size!: number
+
+  @Prop({ default: () => 0 }) private re!: number
+
+  @Prop({ default: () => 0 }) private im!: number
+
+  arcs = range(8).map((i) => {
+    const r = this.rScale(1);
+    const alpha = (2 * Math.PI * i) / 8;
+    const beta = Math.PI / 8;
+    return {
+      x0: r * Math.cos(alpha - beta),
+      y0: r * Math.sin(alpha - beta),
+      x1: r * Math.cos(alpha + beta),
+      y1: r * Math.sin(alpha + beta),
+      re: Math.cos(alpha),
+      im: Math.sin(alpha),
+    };
+  })
+
+  get selectedNonzero(): boolean {
+    return this.re !== 0 || this.im !== 0;
+  }
+
+  get selectedEntryPhaseId(): number {
+    const phi = Math.atan2(this.im, this.re);
+    return (Math.round((8 * phi) / (2 * Math.PI)) + 8) % 8;
+  }
+
+  get selectedPhaseTau(): number {
+    const phi = Math.atan2(this.im, this.re);
+    return phi / (2 * Math.PI);
+  }
+
+  scale(i: number): number {
+    return i * this.size;
+  }
+
+  generateColor(re: number, im: number): string {
+    return colorComplex(re, im);
+  }
+
+  rScale(re: number, im = 0): number {
+    return 0.5 * this.size * Math.sqrt(re ** 2 + im ** 2);
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.label {
+  font-size: 12px;
+  text-anchor: middle;
+  fill: rgba(255, 255, 255, 0.5);
+  cursor: default;
+  text-transform: uppercase;
+  font-weight: 300;
+}
+
+.radius-reference {
+  fill: white;
+  opacity: 0.25;
+}
+
+.radius-value {
+  fill: white;
+}
+
+.phase-arc {
+  stroke: rgba(0, 0, 0, 0.1);
+  stroke-width: 0.5px;
+}
+</style>
--- src/lib-components/quantum-matrix.vue
+++ src/lib-components/quantum-matrix.vue
@@ -139,43 +139,6 @@
             @mouseover="tileMouseOver(d)"
           />
         </g>
-        <!-- <g class="legend" :transform="`translate(${scale(6)}, ${columnSize + scale(4)})`">
-          <g>
-            <text class="label" :x="0" :y="0">
-              Amplitude
-            </text>
-            <circle class="radius-reference" :cx="scale(0)" :cy="scale(1)" :r="rScale(1)" />
-            <circle
-              class="radius-value"
-              :cx="scale(0)"
-              :cy="scale(1)"
-              :r="selectedNonzero ? rScale(selectedEntry.re, selectedEntry.im) : rScale(1)"
-            />
-            <text v-if="selectedNonzero" class="label" :x="rScale(0)" :y="scale(2)">
-              {{ Math.sqrt(selectedEntry.re ** 2 + selectedEntry.im ** 2 || 0).toFixed(3) }}
-            </text>
-          </g>
-          <g :transform="`translate(${scale(3)}, 0)`">
-            <text class="label" :x="0" :y="0">
-              Phase
-            </text>
-            <g :transform="`translate(0, ${scale(1)})`">
-              <path
-                v-for="(a, i) in arcs"
-                :key="`arc-${i}`"
-                class="phase-arc"
-                :style="{
-                  fill: generateColor(a.re, a.im),
-                  opacity: !selectedNonzero || selectedEntryPhaseId === i ? 1 : 0.25
-                }"
-                :d="`M 0 0 L ${a.x0} ${a.y0} A ${rScale(1)} ${rScale(1)} 0 0 1 ${a.x1} ${a.y1} Z`"
-              />
-            </g>
-            <text v-if="selectedNonzero" class="label" :x="rScale(0)" :y="scale(2)">
-              {{ selectedPhaseTau.toFixed(2) }} 𝛕
-            </text>
-          </g>
-        </g> -->
       </svg>
     </div>
     <div class="legend-container">
@@ -209,47 +172,7 @@
         </div>
       </div>
       <div class="matrix-legend" >
-        <svg class="quantum-matrix"
-        :width="300"
-        :height="100">
-          <g class="legend" :transform="`translate(35, 20)`">
-            <g>
-              <text class="label" :x="0" :y="0">
-                Amplitude
-              </text>
-              <circle class="radius-reference" :cx="scale(0)" :cy="scale(1)" :r="rScale(1)" />
-              <circle
-                class="radius-value"
-                :cx="scale(0)"
-                :cy="scale(1)"
-                :r="selectedNonzero ? rScale(selectedEntry.re, selectedEntry.im) : rScale(1)"
-              />
-              <text v-if="selectedNonzero" class="label" :x="rScale(0)" :y="scale(2)">
-                {{ Math.sqrt(selectedEntry.re ** 2 + selectedEntry.im ** 2 || 0).toFixed(3) }}
-              </text>
-            </g>
-            <g :transform="`translate(${scale(3)}, 0)`">
-              <text class="label" :x="0" :y="0">
-                Phase
-              </text>
-              <g :transform="`translate(0, ${scale(1)})`">
-                <path
-                  v-for="(a, i) in arcs"
-                  :key="`arc-${i}`"
-                  class="phase-arc"
-                  :style="{
-                    fill: generateColor(a.re, a.im),
-                    opacity: !selectedNonzero || selectedEntryPhaseId === i ? 1 : 0.25
-                  }"
-                  :d="`M 0 0 L ${a.x0} ${a.y0} A ${rScale(1)} ${rScale(1)} 0 0 1 ${a.x1} ${a.y1} Z`"
-                />
-              </g>
-              <text v-if="selectedNonzero" class="label" :x="rScale(0)" :y="scale(2)">
-                {{ selectedPhaseTau.toFixed(2) }} 𝛕
-              </text>
-            </g>
-          </g>
-        </svg>
+        <complex-legend :re="selectedEntry.re" :im="selectedEntry.im"/>
       </div>
     </div>
   </div>
@@ -258,9 +181,9 @@
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import { Operator } from 'quantum-tensors';
-import { range } from '@/lib-components/utils';
 import { colorComplex } from '@/lib-components/colors';
 import ViewerButton from '@/lib-components/viewer-button.vue';
+import ComplexLegend from '@/lib-components/complex-legend.vue';
 
 interface IMatrixElement {
   i: number
@@ -272,10 +195,11 @@ interface IMatrixElement {
 @Component({
   components: {
     ViewerButton,
+    ComplexLegend,
   },
 })
 
-export default class QuanutmMatrix extends Vue {
+export default class QuantumMatrix extends Vue {
   @Prop({ default: () => 40 }) private size!: number
 
   @Prop({ default: () => [[]] }) private operator!: Operator
@@ -310,34 +234,6 @@ export default class QuanutmMatrix extends Vue {
 
   selectedEntry: IMatrixElement = {
     i: -1, j: -1, re: 0, im: 0,
-  }
-
-  arcs = range(8).map((i) => {
-    const r = this.rScale(1);
-    const alpha = (2 * Math.PI * i) / 8;
-    const beta = Math.PI / 8;
-    return {
-      x0: r * Math.cos(alpha - beta),
-      y0: r * Math.sin(alpha - beta),
-      x1: r * Math.cos(alpha + beta),
-      y1: r * Math.sin(alpha + beta),
-      re: Math.cos(alpha),
-      im: Math.sin(alpha),
-    };
-  })
-
-  get selectedNonzero(): boolean {
-    return this.selectedEntry.re !== 0 || this.selectedEntry.im !== 0;
-  }
-
-  get selectedEntryPhaseId(): number {
-    const phi = Math.atan2(this.selectedEntry.im, this.selectedEntry.re);
-    return (Math.round((8 * phi) / (2 * Math.PI)) + 8) % 8;
-  }
-
-  get selectedPhaseTau(): number {
-    const phi = Math.atan2(this.selectedEntry.im, this.selectedEntry.re);
-    return phi / (2 * Math.PI);
   }
 
   get selectedInLabelOne(): string {
@@ -491,7 +387,7 @@ export default class QuanutmMatrix extends Vue {
 
 .entry-tile,
 .menu-tile {
-  fill: none;
+  fill: rgba(0, 0, 0, 0);
   stroke: rgba(255, 255, 255, 0.1);
   stroke-width: 1px;
 }
@@ -510,19 +406,5 @@ export default class QuanutmMatrix extends Vue {
 .tile-value:hover {
   stroke: rgb(255, 255, 255);
   stroke-width: 2px;
-}
-
-.radius-reference {
-  fill: white;
-  opacity: 0.25;
-}
-
-.radius-value {
-  fill: white;
-}
-
-.phase-arc {
-  stroke: rgba(0, 0, 0, 0.1);
-  stroke-width: 0.5px;
 }
 </style>
