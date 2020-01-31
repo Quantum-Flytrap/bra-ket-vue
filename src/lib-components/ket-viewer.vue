1fd21771369e1f99e289a9ea2366b03e09d27d8f
Klem <k.jankiewicz@gmail.com>
2020-01-31 14:42:24 +0100

    fixed - ket responding to view change

--- src/lib-components/ket-viewer.vue
+++ src/lib-components/ket-viewer.vue
@@ -145,9 +145,9 @@ export default class KetViewer extends Vue {
 
   @Prop({ default: true }) readonly showTable!: boolean
 
-  styles = ['polar', 'cartesian', 'color']
+  @Prop({ default: 'polar' }) readonly selectedStyle!: string
 
-  selectedStyle = 'polar'
+  styles = ['polar', 'cartesian', 'color']
 
   toPercent(x: number, precision = 1): string {
     return (100 * x).toFixed(precision);
@@ -258,7 +258,7 @@ export default class KetViewer extends Vue {
     justify-content: center;
     max-width: 100%;
     margin-bottom: 5px;
-    margin-bottom: 10px;
+    padding: 5px;
     border-bottom: 1px solid rgba(255, 255, 255, 0.5);
   }
   @media screen and (max-width: 1000px) {
@@ -266,7 +266,7 @@ export default class KetViewer extends Vue {
   }
 }
 .legend {
-  margin-top: 10px;
+  margin-top: 5px;
   border-top: 1px solid rgba(255, 255, 255, 0.5);
 }
 
