53cf911eb7e0d49c19b03f1555a3326cc3dbea92
Piotr Migdal <pmigdal@gmail.com>
2020-02-20 14:35:13 +0100

    feat(matrix) prettier utf8 coords

--- src/lib-components/utils.ts
+++ src/lib-components/utils.ts
@@ -1,2 +1,9 @@
-/* eslint-disable import/prefer-default-export */
 export const range = (n: number): number[] => [...Array(n).keys()];
+
+export const coordPrettyMap = new Map<string, string>();
+const pairs: [string, string][] = [['>', '→'], ['^', '↑'], ['<', '←'], ['v', '↓']];
+pairs.forEach(([k, v]) => coordPrettyMap.set(k, v));
+
+export function coordPrettier(coord: string): string {
+  return coordPrettyMap.get(coord) || coord;
+}
