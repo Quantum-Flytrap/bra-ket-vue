b66fa286fb7eb73013e0c8c5bd9a510e34002d86
Piotr Migdal <pmigdal@gmail.com>
2020-02-21 21:47:35 +0100

    feat(rollup) export all components

--- src/lib-components/index.ts
+++ src/lib-components/index.ts
@@ -1,3 +1,20 @@
-/* eslint-disable import/prefer-default-export */
+// core
 export { default as KetViewer } from './ket-viewer.vue';
 export { default as MatrixViewer } from './matrix-viewer.vue';
+
+// higher-level
+export { default as KetListViewer } from './ket-list-viewer.vue';
+
+// subcomponents, potentially useful in general
+export { default as ComplexLegend } from './complex-legend.vue';
+
+// subcomponents, ket
+export { default as CoordinateLegend } from './coordinate-legend.vue';
+
+// subcomponents, matrix
+export { default as MatrixDimensions } from './matrix-dimensions.vue';
+export { default as MatrixLabels } from './matrix-labels.vue';
+
+// subcomponents, mixed
+export { default as ViewerButton } from './viewer-button.vue';
+export { default as ViewButtonGroup } from './view-button-group.vue';
