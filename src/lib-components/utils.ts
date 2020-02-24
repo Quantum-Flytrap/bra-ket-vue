507b4b18be9ec5885803b5e2c3ad94c11ea5504b
Klem <k.jankiewicz@gmail.com>
2020-02-24 15:36:31 +0100

    spin notation style

--- src/lib-components/utils.ts
+++ src/lib-components/utils.ts
@@ -1,7 +1,10 @@
 export const range = (n: number): number[] => [...Array(n).keys()];
 
 export const coordPrettyMap = new Map<string, string>();
-const pairs: [string, string][] = [['>', '→'], ['^', '↑'], ['<', '←'], ['v', '↓']];
+const pairs: [string, string][] = [
+  ['>', '→'], ['^', '↑'], ['<', '←'], ['v', '↓'],
+  ['u', '↑z'], ['d', '↓z'], ['ux', '↑x'], ['dx', '↓x'], ['uy', '↑y'], ['dy', '↓y'],
+];
 pairs.forEach(([k, v]) => coordPrettyMap.set(k, v));
 
 export function coordPrettier(coord: string): string {
