b64c36086fb85b20e7f2c7d92bb14617cd70d853
Piotr Migdal <pmigdal@gmail.com>
2020-03-28 16:30:38 +0100

    refactor(ket) one less nesting

--- src/lib-components/ket.vue
+++ src/lib-components/ket.vue
@@ -1,48 +1,43 @@
 <template>
   <div
-    ref="wrapper"
-    class="ket"
+    class="quantum-state-viewer"
   >
-    <div
-      class="quantum-state-viewer"
+    <span
+      v-for="(ketComponent, index) in ketComponents"
+      :key="`ket-component-${index}`"
+      :class="ketComponentClass"
     >
       <span
-        v-for="(ketComponent, index) in ketComponents"
-        :key="`ket-component-${index}`"
-        :class="ketComponentClass"
+        v-if="selectedOption !== 'color'"
+        class="ket-complex"
       >
-        <span
-          v-if="selectedOption !== 'color'"
-          class="ket-complex"
-        >
-          {{ ketComponent.amplitude.toString(selectedOption) }}
-        </span>
-        <svg
-          v-if="selectedOption === 'color'"
-          height="1em"
-          width="1em"
-          viewBox="0 0 16 16"
-          class="ket-disk"
-        >
-          <circle
-            cx="8"
-            cy="8"
-            :r="discScale(ketComponent.amplitude.r)"
-            :fill="complexToColor(ketComponent.amplitude)"
-          />
-        </svg>
-        <span class="ket-coords">
-          <span class="ket-parenthesis">|</span>
-          <span
-            v-for="(coordStr, i) in ketComponent.coordStrs"
-            :key="`ket-component-${i}-${coordStr}`"
-            class="ket-coord"
-            :style="{ color: dimensionNameToColor(dimensionNames[i], darkMode) }"
-          >{{ coordPrettier(coordStr) }}</span>
-          <span class="ket-parenthesis">⟩</span>
-        </span>
+        {{ ketComponent.amplitude.toString(selectedOption) }}
       </span>
-    </div>
+      <svg
+        v-if="selectedOption === 'color'"
+        height="1em"
+        width="1em"
+        viewBox="0 0 16 16"
+        class="ket-disk"
+      >
+        <circle
+          cx="8"
+          cy="8"
+          :r="discScale(ketComponent.amplitude.r)"
+          :fill="complexToColor(ketComponent.amplitude)"
+        />
+      </svg>
+      <span class="ket-coords">
+        <span class="ket-parenthesis">|</span>
+        <span
+          v-for="(coordStr, i) in ketComponent.coordStrs"
+          :key="`ket-component-${i}-${coordStr}`"
+          class="ket-coord"
+          :style="{ color: dimensionNameToColor(dimensionNames[i], darkMode) }"
+        >{{ coordPrettier(coordStr) }}</span>
+        <span class="ket-parenthesis">⟩</span>
+      </span>
+    </span>
   </div>
 </template>
 
@@ -139,54 +134,48 @@ export default Vue.extend({
 <style lang="scss" scoped>
 @import "../style-variables.scss";
 
-.ket {
+.quantum-state-viewer {
   display: flex;
-  transition: height 0.5s;
-  overflow: hidden;
-  align-content: space-between;
+  flex-wrap: wrap;
+  font-size: 12px;
   font-family: $ketFont;
-  & .quantum-state-viewer {
+  & .ket-component-dark, .ket-component-bright {
+    margin: 2px 6px 0px 0px;
+    padding: 3px 4px;
+    flex-wrap: nowrap;
+    flex-direction: row;
     display: flex;
-    flex-wrap: wrap;
-    font-size: 12px;
-    & .ket-component-dark, .ket-component-bright {
-      margin: 2px 6px 0px 0px;
-      padding: 3px 4px;
-      flex-wrap: nowrap;
-      flex-direction: row;
-      display: flex;
-      align-items: center;
-      & .ket-complex {
-        margin-right: 2px;
-      }
-      & .ket-coords {
-        display: inline-flex;
-      }
-      & .ket-coord {
-        padding-right: 1px;
-        padding-right: 3px;
-      }
+    align-items: center;
+    & .ket-complex {
+      margin-right: 2px;
     }
-    & .ket-component-dark {
-      background-color: rgba(0, 0, 0, 0.3);
-      & .ket-complex {
-        color: #d28fff;
-      }
-      & .ket-coords {
-        color: #fff;
-      }
+    & .ket-coords {
+      display: inline-flex;
     }
-    & .ket-component-bright {
-      border-width: 1px;
-      border-style: solid;
-      border-color: rgb(240, 240, 240);
-      background-color: white;
-      & .ket-complex {
-        color: #7a06c7;
-      }
-      & .ket-coords {
-        color: #242424;
-      }
+    & .ket-coord {
+      padding-right: 1px;
+      padding-right: 3px;
+    }
+  }
+  & .ket-component-dark {
+    background-color: rgba(0, 0, 0, 0.3);
+    & .ket-complex {
+      color: #d28fff;
+    }
+    & .ket-coords {
+      color: #fff;
+    }
+  }
+  & .ket-component-bright {
+    border-width: 1px;
+    border-style: solid;
+    border-color: rgb(240, 240, 240);
+    background-color: white;
+    & .ket-complex {
+      color: #7a06c7;
+    }
+    & .ket-coords {
+      color: #242424;
     }
   }
 }
